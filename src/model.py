c75016edecb571e3938c0adedcd25eb492c5ee88
CarlosPerez <carlosmperez2901@gmail.com>
2024-09-07 20:50:47 -0700

    First commit

---
+++ src/model.py
@@ -0,0 +1,8 @@
+from src.status import Status
+from datetime import datetime
+class Task:
+    task_id:str
+    description:str
+    status:Status
+    created_at:datetime
+    updated_at:datetime
---
+++ src/status.py
@@ -0,0 +1,4 @@
+class Status():
+    todo:str = 'TODO'
+    in_progress:str = 'IN PROGRESS'
+    done:str = 'DONE'
