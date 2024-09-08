d4856163229b536e05310775f716705318edf9a9
CarlosPerez <carlosmperez2901@gmail.com>
2024-09-07 21:53:29 -0700

    Added 'add' functionality

--- src/app.py
+++ src/app.py
@@ -1,11 +1,24 @@
-import argparse
+from argparse import ArgumentParser
+import os
+
+from src.model import Task
+
+json_file_path:str = os.getcwd()+'/tasks.json'
+
+def add(description:str)->None:
+    with open(json_file_path,'a') as file:
+        task = Task(description)
+        file.write(task.description+'\n')
+
+    print('Task Added Successfully')
 
 
-def add()->None:...
-def update()->None:...
-def delete()->None:...
-def done()->None:...
-def in_progress()->None:...
+
+def update(task_id:str)->None:...
+def delete(task_id:str)->None:...
+def mark_as_done(task_id:str)->None:...
+def mark_as_in_progress(task_id:str)->None:...
+
 def all_tasks()->None:...
 def all_done()->None:...
 def all_not_done()->None:...
@@ -17,7 +30,17 @@ def all_in_progress()->None:...
 
 
 def main()->None:
-    print('working on it...')
-    
+    parser:ArgumentParser = ArgumentParser()
+    subparser = parser.add_subparsers(dest='command')
+
+    parser_add:ArgumentParser = subparser.add_parser('add',help='add a new task')
+    parser_add.add_argument('task_description', help='Add a description to the task')
+
+    args = parser.parse_args()
+
+    if args.command=='add':
+        add(args.task_description)
+    else:
+        parser.print_help()
 
 
--- src/model.py
+++ src/model.py
@@ -1,8 +1,17 @@
 from src.status import Status
 from datetime import datetime
+
 class Task:
-    task_id:str
-    description:str
-    status:Status
-    created_at:datetime
-    updated_at:datetime
+    __counter:int = 0
+
+
+    def __init__(self,description:str,status:Status=Status.todo)->None:
+        self.description:str=description
+        self.status:Status=status
+        created_at:datetime= datetime.now()
+        updated_at:datetime=datetime.now()
+        Task.__counter +=1
+        self.task_id:int = Task.__counter
+
+
+    
