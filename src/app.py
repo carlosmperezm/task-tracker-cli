46fe0462929df7e5c30fbe902ff1b1db80aa26bf
CarlosPerez <carlosmperez2901@gmail.com>
2024-09-08 21:57:17 -0700

    Now add method and list method works better

--- src/app.py
+++ src/app.py
@@ -1,25 +1,61 @@
+from typing import Any,Dict,List
 from argparse import ArgumentParser
 import os
+import json
 
 from src.model import Task
 
-json_file_path:str = os.getcwd()+'/tasks.json'
+JSON_FILE_PATH:str = os.getcwd()+'/tasks.json'
+
+def write_json(data:Dict[str,Any],file_path:str = JSON_FILE_PATH)->None:
+    with open(file_path, 'w') as file:
+        json.dump(data, file)
 
 def add(description:str)->None:
-    with open(json_file_path,'a') as file:
-        task = Task(description)
-        file.write(task.description+'\n')
+    task_id:int=1
+    data:dict[str,Any]={}
+    tasks:list[dict[str,Any] | None]= []
 
+    tasks = all_tasks()
+    if tasks != []:
+        task_id = tasks[-1].get('task_id') + 1
+
+    task = Task(task_id,description)
+    tasks.append(task.to_dict())
+
+    write_json(tasks)
     print('Task Added Successfully')
 
 
 
 def update(task_id:str)->None:...
-def delete(task_id:str)->None:...
+
+def delete(task_id:str)->str:
+    # TODO: it's not working
+    tasks:list[dict[str,Any] | None]= all_tasks()
+
+    for index,task in enumerate(tasks.copy()):
+        if task.get('task_id') == task_id:
+            item_deleted = tasks.pop(index)
+            write_json(tasks)
+            return f'Successfully deleted item: {item_deleted.get('task_id')}'
+    return 'No indexes found'
+
+
 def mark_as_done(task_id:str)->None:...
 def mark_as_in_progress(task_id:str)->None:...
 
-def all_tasks()->None:...
+def all_tasks()->List[Dict[str,Any] | None]:
+    data:list[dict[str,Any]]=[]
+    with open(JSON_FILE_PATH) as file:
+        try:
+            data = json.load(file)
+        except json.JSONDecodeError :
+            pass
+            
+    return data
+
+
 def all_done()->None:...
 def all_not_done()->None:...
 def all_in_progress()->None:...
@@ -33,13 +69,24 @@ def main()->None:
     parser:ArgumentParser = ArgumentParser()
     subparser = parser.add_subparsers(dest='command')
 
-    parser_add:ArgumentParser = subparser.add_parser('add',help='add a new task')
+    parser_add:ArgumentParser = subparser.add_parser('add',help='Add a new task')
     parser_add.add_argument('task_description', help='Add a description to the task')
+
+    parser_list:ArgumentParser = subparser.add_parser('list', help='Shows all the tasks')
+
+    parser_delete:ArgumentParser = subparser.add_parser('delete',help='Delete the task when an id is provided')
+    parser_delete.add_argument('task_id', help='Provide the id of the specific id you want to delete')
 
     args = parser.parse_args()
 
     if args.command=='add':
         add(args.task_description)
+    elif args.command =='list':
+        print(all_tasks())
+    elif args.command == 'delete':
+        print
+        delete(args.task_id)
+
     else:
         parser.print_help()
 
--- src/model.py
+++ src/model.py
@@ -1,17 +1,46 @@
+from typing import Any
 from src.status import Status
 from datetime import datetime
 
 class Task:
-    __counter:int = 0
 
-
-    def __init__(self,description:str,status:Status=Status.todo)->None:
+    def __init__(self,task_id:int,description:str,status:Status=Status.todo)->None:
         self.description:str=description
         self.status:Status=status
-        created_at:datetime= datetime.now()
-        updated_at:datetime=datetime.now()
-        Task.__counter +=1
-        self.task_id:int = Task.__counter
+        self.created_at:dict[str,Any] = self.created_at()
+        self.updated_at:dict[str,Any] = self.updated_at()
+        #####################################################
+
+        self.task_id:int = task_id
+        return super().__init__()
 
 
+    def to_dict(self)->dict:
+        return dict(
+                task_id=self.task_id,
+                status=self.status,
+                description=self.description,
+                created_at=self.created_at,
+                updated_at=self.updated_at
+             )
     
+    def created_at(self)->dict[str,Any]:
+        # TODO: Check if works
+        now:datetime = datetime.now()
+        date:dict[str,Any] = {
+                'year':now.year,
+                'month':now.month,
+                'day':now.day,
+                'hour':now.hour,
+                'minute':now.minute,
+                'second':now.second,
+                'microsecond':now.microsecond,
+                }
+        return date
+
+
+    def updated_at(self)->dict[str,Any]:
+        # TODO: Check if works
+       return self.created_at
+
+
